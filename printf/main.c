8e62a3aca0a9b2d16ce424dc3d7c315e9688888a
Arnaud GISSINGER <agissing@e3r10p8.42.fr>
2018-12-10 19:56:54 +0100

    Auto commit Mon Dec 10 18:36:42 CET 2018

--- printf/main.c
+++ printf/main.c
@@ -6,7 +6,7 @@
 /*   By: agissing <agissing@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/08 17:10:46 by agissing          #+#    #+#             */
-/*   Updated: 2018/12/10 12:22:58 by agissing         ###   ########.fr       */
+/*   Updated: 2018/12/10 18:51:47 by agissing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int		main(void)
 	char		nb;
 	long double	dd;
 
-	dd = -9999912.0599999999999999999999999;
+	dd = 1.3e+300;
 	nb = 950;
-	printf(" = %d\n", ft_printf("%d %hx %100p", nb, nb, &dd));
-	printf(" = %d\n", printf("%d %hx %100p", nb, nb, &dd));
+	printf(" = %d\n", ft_printf(".30%Lf", dd));
+	printf(" = %d\n", printf("%.30Lf", dd));
 	return (0);
 }
--- printf/srcs/put_double.c
+++ printf/srcs/put_double.c
@@ -6,7 +6,7 @@
 /*   By: agissing <agissing@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/09 16:05:27 by agissing          #+#    #+#             */
-/*   Updated: 2018/12/10 11:40:47 by agissing         ###   ########.fr       */
+/*   Updated: 2018/12/10 19:02:04 by agissing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int		ft_put_ldouble(long double nb, int precision)
 	int		count;
 
 	count = 0;
-	count += ft_putnb((int)nb, 10, "0123456789");
+	count += ft_putnb((long long)nb, 10, "0123456789");
 	count += ft_putchar('.');
 	nb = (nb < 0) ? -nb : nb;
 	while (precision-- > 0)
