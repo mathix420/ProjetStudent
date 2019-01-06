f84404bf8cb771ab5f415b4fab84fe8fde11bd03
Arnaud GISSINGER <agissing@e2r4p3.42.fr>
2019-01-06 18:55:28 +0100

    Auto commit Sun Jan  6 16:59:20 CET 2019

--- push_swap/push_swap_srcs/push_swap.h
+++ push_swap/push_swap_srcs/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: agissing <agissing@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/04 14:04:57 by agissing          #+#    #+#             */
-/*   Updated: 2019/01/05 20:06:10 by agissing         ###   ########.fr       */
+/*   Updated: 2019/01/06 18:14:10 by agissing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,9 @@ typedef struct	s_sort
 	t_op	**op;
 }				t_sort;
 
+int				ft_moy(t_stack *pile, int max);
+int				is_ok_a(t_stack *pile, int max);
+int				is_ok_b(t_stack *pile, int max);
 void			sort_a(t_stack **pila, t_stack **pilb, t_sort v);
 void			sort_b(t_stack **pila, t_stack **pilb, t_sort v);
 
---
+++ push_swap/push_swap_srcs/small_sort.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   small_sort.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agissing <agissing@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/06 18:03:37 by agissing          #+#    #+#             */
+/*   Updated: 2019/01/06 18:23:34 by agissing         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int		
+
+void	push_tri_a(t_stack **pila, t_stack **pilb, t_sort v)
+{
+
+}
+
+void	top_sort_b(t_stack **pila, t_stack **pilb, t_sort v)
+{
+	moy = ft_moy(*pilb, c);
+	if (v.bc > 1 && (*pilb)->nb < moy)
+		*pilb = rotate(*pilb);
+}
+
+void	small_sort(t_stack **pila, t_stack **pilb, t_sort v)
+{
+	while (*pila)
+	{
+		while (!is_ok_a(*pila, v.c))
+		*pila = (*pila)->before;
+	}
+}
