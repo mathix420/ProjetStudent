64344c19e5c06493cff25bdc08c58124fc353878
Arnaud GISSINGER <agissing@e3r9p8.42.fr>
2019-01-10 17:19:24 +0100

    Auto commit Thu Jan 10 12:31:15 CET 2019

--- push_swap/includes/checker.h
+++ push_swap/includes/checker.h
@@ -6,7 +6,7 @@
 /*   By: agissing <agissing@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/19 13:04:53 by agissing          #+#    #+#             */
-/*   Updated: 2019/01/09 22:03:00 by agissing         ###   ########.fr       */
+/*   Updated: 2019/01/10 17:18:40 by agissing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,32 @@
 # include "stack.h"
 # include "mlx.h"
 
-typedef struct	s_mlx
-{
-	void    *mlx;
-	void    *win;
-	int		siz_x;
-	int		siz_y;
-	int		color;
-}				t_mlx;
-
 typedef struct	s_off
 {
 	int		x;
 	int		y;
 }				t_off;
+
+typedef struct	s_op
+{
+	int			nb;
+	struct s_op	*next;
+}				t_op;
+
+typedef struct	s_mlx
+{
+	void    *mlx;
+	void    *win;
+	void    *img;
+	t_stack *pila;
+	t_stack *pilb;
+	int		*str;
+	int		siz_x;
+	int		siz_y;
+	int		color;
+	int		ep;
+	int		step;
+	t_op	*op;
+}				t_mlx;
 
 #endif
--- push_swap/srcs/checker.c
+++ push_swap/srcs/checker.c
@@ -6,7 +6,7 @@
 /*   By: agissing <agissing@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/19 13:04:24 by agissing          #+#    #+#             */
-/*   Updated: 2019/01/10 12:25:55 by agissing         ###   ########.fr       */
+/*   Updated: 2019/01/10 17:19:13 by agissing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,48 +39,174 @@ int		ft_len(t_stack *pile)
 	return (i);
 }
 
-void    ft_fill_pixel(t_mlx mlx, int x, int y, int color)
+void	ft_add_op(t_op **op, int nb)
 {
-	mlx.str[x + mlx.siz_x * y] = color;
+	t_op	*new;
+	t_op	*tmp;
+
+	if (!*op)
+	{
+		if (!(*op = ft_memalloc(sizeof(t_op))))
+			return ;
+		(*op)->nb = nb;
+		(*op)->next = NULL;
+	}
+	else
+	{
+		if (!(new = ft_memalloc(sizeof(t_op))))
+			return ;
+		new->nb = nb;
+		new->next = NULL;
+		tmp = (*op);
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
 }
 
-void	ligne(t_mlx mlx, t_off off, int ep, int len)
+void	ft_free_ops(t_op *op)
+{
+	t_op	*tmp;
+
+	while (op->next)
+	{
+		tmp = op->next;
+		free(op);
+		op = tmp;
+	}
+	free(op);
+}
+
+void    ft_fill_pixel(t_mlx *mlx, int x, int y, int color)
+{
+	mlx->str[x + mlx->siz_x * y] = color;
+}
+
+void	ligne(t_mlx *mlx, t_off off, int ep, int len)
 {
 	int		i;
 	int		j;
 
-	i = mlx.siz_y - off.y - ep;
-	while (i < mlx.siz_y - off.y)
+	i = mlx->siz_y - off.y - ep;
+	while (i < mlx->siz_y - off.y)
 	{
 		j = off.x;
 		while (j < off.x + len)
-			ft_fill_pixel(mlx.mlx, mlx.win, j++, i, mlx.color);
+			ft_fill_pixel(mlx, j++, i, mlx->color);
 		i++;
 	}	
 }
 
-void	print(t_stack *pile, t_mlx mlx)
+void	print(t_mlx *mlx, t_off off, t_stack *pile)
 {
-	t_off	off;
-	int		ep;
-	int		step;
 	int		i;
 
-	ep = 1000 / ft_len(pile);
-	step = 500 / ft_max(pile);
-	off.x = 0;
 	i = 1;
 	while (pile)
 	{
 		if (pile->nb > 0)
-			mlx.color = 0x00ffff;
+			mlx->color = 0x00ffff;
 		else
-			mlx.color = 0x99ffff;
-		off.y = (i - 1) * ep;
-		ligne(mlx, off, ep, ft_abs(pile->nb) * step);
+			mlx->color = 0x99ffff;
+		off.y = (i - 1) * mlx->ep;
+		ligne(mlx, off, mlx->ep, ft_abs(pile->nb) * mlx->step);
 		pile = pile->before;
 		i++;
 	}
+}
+
+void	print_both(t_mlx *mlx)
+{
+	t_off	off;
+
+	off.x = 0;
+	print(mlx, off, mlx->pila);
+	off.x = 505;
+	print(mlx, off, mlx->pilb);
+}
+
+t_op	*ft_do_op(t_mlx *mlx)
+{
+	t_op	*tmp;
+
+	if (mlx->op->nb == 1 || mlx->op->nb == 2)
+		swap(mlx->op->nb == 1 ? mlx->pila : mlx->pilb);
+	else if (mlx->op->nb == 3)
+		ss(mlx->pila, mlx->pilb);
+	else if (mlx->op->nb == 4 || mlx->op->nb == 5)
+		mlx->op->nb == 4 ? mlx->pila = rotate(mlx->pila) :
+			(mlx->pilb = rotate(mlx->pilb));
+	else if (mlx->op->nb == 6)
+		rr(&mlx->pila, &mlx->pilb);
+	else if (mlx->op->nb == 7 || mlx->op->nb == 8)
+		mlx->op->nb == 7 ? mlx->pila = reverse(mlx->pila) :
+			(mlx->pilb = reverse(mlx->pilb));
+	else if (mlx->op->nb == 9)
+		rrr(&mlx->pila, &mlx->pilb);
+	else if (mlx->op->nb == 10 || mlx->op->nb == 11)
+		mlx->op->nb == 11 ? push(&mlx->pila, &mlx->pilb) :
+			push(&mlx->pilb, &mlx->pila);
+	tmp = mlx->op->next;
+	free(mlx->op);
+	return (tmp);
+}
+
+int		treat(t_mlx *mlx)
+{
+	int		i;
+
+	i = 0;
+	if (!mlx->op)
+		return (0);
+	print_both(mlx);
+	printf("%d\n", mlx->op->nb);
+	mlx->op = ft_do_op(mlx);
+	print_stack(mlx->pila);
+	ft_bzero(mlx->str, mlx->siz_x * mlx->siz_y);
+	print_both(mlx);
+	mlx_put_image_to_window(mlx->mlx, mlx->win, mlx->img, 0, 0);
+	return (0);
+}
+
+int		ft_add(t_op **op, char *ops)
+{
+	if (!ft_strcmp("sa", ops))
+		ft_add_op(op, 1);
+	else if (!ft_strcmp("sb", ops))
+		ft_add_op(op, 2);
+	else if (!ft_strcmp("ss", ops))
+		ft_add_op(op, 3);
+	else if (!ft_strcmp("ra", ops))
+		ft_add_op(op, 4);
+	else if (!ft_strcmp("rb", ops))
+		ft_add_op(op, 5);
+	else if (!ft_strcmp("rr", ops))
+		ft_add_op(op, 6);
+	else if (!ft_strcmp("rra", ops))
+		ft_add_op(op, 7);
+	else if (!ft_strcmp("rrb", ops))
+		ft_add_op(op, 8);
+	else if (!ft_strcmp("rrr", ops))
+		ft_add_op(op, 9);
+	else if (!ft_strcmp("pa", ops))
+		ft_add_op(op, 10);
+	else if (!ft_strcmp("pb", ops))
+		ft_add_op(op, 11);
+	else
+		return (0);
+	return (1);
+}
+
+void	ft_get_op(t_op **op)
+{
+	char	*ops;
+
+	ops = NULL;
+	while (get_next_line(0, &ops))
+		if (!ft_add(op, ops))
+			ft_putstr("Error TODO !!!!!!");
+	if (ops)
+		free(ops);
 }
 
 int		main(int c, char **v)
@@ -99,10 +225,28 @@ int		main(int c, char **v)
 	while (i >= 1)
 		if (!(stck_a = ft_new_elem(ft_atoi(v[i--]), stck_a)))
 			return (0);
+
+	ft_get_op(&mlx.op);
+
 	mlx.mlx = mlx_init();
-	mlx.siz_x = 1000;
+	mlx.siz_x = 1005;
 	mlx.siz_y = 1000;
 	mlx.win = mlx_new_window(mlx.mlx, mlx.siz_x, mlx.siz_y, "Checker Push_swap");
-	print(stck_a, mlx);
+	mlx.pila = stck_a;
+	mlx.pilb = stck_b;
+
+    int     bpp;
+    int     s_l;
+    int     endian;
+	
+	mlx.img = mlx_new_image(mlx.mlx, mlx.siz_x, mlx.siz_y);
+	mlx.str = (int *)mlx_get_data_addr(mlx.img, &bpp, &s_l, &endian);
+	
+	mlx.ep = 1000 / ft_len(stck_a);
+	mlx.step = 500 / ft_max(stck_a);
+
+	print_both(&mlx);
+
+	mlx_loop_hook(mlx.mlx, &treat, &mlx);
 	mlx_loop(mlx.mlx);
 }
