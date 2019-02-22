2fbabab242122723198729e2c5ea45babccd3cdd
Arnaud GISSINGER <agissing@e2r11p13.42.fr>
2019-02-22 18:13:31 +0100

    test

--- _tests/init_exe_creator.sh
+++
@@ -1,4 +0,0 @@
-echo "#!/bin/sh\n$1\n\nWrite in $2"
-echo "#!/bin/sh\n$1" > $2
-find . \( -name '*~' -type f -or -name '#*#' -type f -or -name 'a.out' -type f -or -name 'a.out.dSYM' -type d \) | xargs rm -rf
-chmod +x $2
