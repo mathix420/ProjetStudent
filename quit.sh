c6a56c765b160c408e10685c5af87a5f2478e9fb
Arnaud GISSINGER <agissing@e2r11p10.42.fr>
2019-02-28 19:10:12 +0100

    Auto commit Thu Feb 28 19:10:11 CET 2019

--- quit.sh
+++ quit.sh
@@ -6,7 +6,7 @@
 #    By: agissing <agissing@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/02/22 22:17:48 by agissing          #+#    #+#              #
-#    Updated: 2019/02/22 22:22:39 by agissing         ###   ########.fr        #
+#    Updated: 2019/02/28 19:09:40 by agissing         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -29,4 +29,5 @@ python3 ~/ratpy.py
 sleep 15
 
 osascript -e 'tell app "System Events" to log out'
+sleep 1
 osascript -e 'tell application "System Events" to keystroke RETURN'
