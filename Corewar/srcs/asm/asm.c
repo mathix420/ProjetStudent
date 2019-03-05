305ade7ac79d352feaa40edf9d198a829f4bad80
Arnaud GISSINGER <agissing@e3r2p20.42.fr>
2019-03-05 15:52:30 +0100

    Auto commit Tue Mar  5 15:52:30 CET 2019

--- Corewar/srcs/asm/asm.c
+++ Corewar/srcs/asm/asm.c
@@ -6,7 +6,7 @@
 /*   By: trlevequ <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 18:12:00 by trlevequ          #+#    #+#             */
-/*   Updated: 2019/03/05 14:52:40 by agissing         ###   ########.fr       */
+/*   Updated: 2019/03/05 15:52:07 by agissing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,13 +62,35 @@ static char				*get_name(char *str)
 
 static int				get_param(char *str, int index)
 {
+	int		ok;
 	int		i;
+	int		j;
 
 	i = -1;
-	while (str[++i])
+	ok = -1;
+	(void)index;
+	while (str[++i] || ok >= 0)
 	{
-		
+		if (str[i] == '#')
+			return (1);
+		else if (ok == -1 && (str[i] == DIRECT_CHAR || str[i] == 'r'))
+			ok = i;
+		else if (ok >= 0 && (str[i] == SEPARATOR_CHAR || is_space(str[i]) || str[i] == '\n' || !str[i]))
+		{
+			ft_putstr(COLOR_GREEN);
+			ft_putstr(op_tab[index].name);
+			ft_putstr(COLOR_END);
+			ft_putstr(" :: new param :: '");
+			j = ok;
+			while (j < i)
+				write(1, &str[j++], 1);
+			ft_putendl("'");
+			ok = -1;
+			if (!str[i])
+				return (1);
+		}
 	}
+	return (1);
 }
 
 static int				get_index(char *str)
@@ -87,7 +109,7 @@ static int				get_index(char *str)
 
 // fonction qui separe tout les args
 
-static int				parse(char *str, int count)
+static int				parse(char *str, int count, t_header *h)
 {
 	char	*tmp;
 	int		i;
@@ -100,45 +122,50 @@ static int				parse(char *str, int count)
 			ft_strlen(COMMENT_CMD_STRING))))
 		p_error(i, str + i, NO_NAME_OR_COMMENT);
 	else if (count < 2)
-		printf(count == 0 ? "NAME :: %s\n" : "COMMENT :: %s\n", str);
-	while (str[++i])
-	{
-		if (str[i] == COMMENT_CHAR)
-			return (1);
-		if (str[i] == LABEL_CHAR && str[i - 1] != DIRECT_CHAR)
+		count == 0 ? ft_strcpy(h->prog_name, str) : ft_strcpy(h->comment, str);
+	else
+		while (str[++i])
 		{
-			printf("There is a label :: %s\n", (tmp = get_name(str)));
-			free(tmp);
+			if (str[i] == COMMENT_CHAR)
+				return (1);
+			if (str[i] == LABEL_CHAR && str[i - 1] != DIRECT_CHAR)
+			{
+				printf("%sThere is a label :: %s%s\n", COLOR_RED, (tmp = get_name(str)), COLOR_END);
+				free(tmp);
+			}
+			else if ((id = get_index(str + i)) >= 0)
+			{
+				printf("Get function :: %s\n", op_tab[id].name);
+				return (1);
+			}
 		}
-		else if ((id = get_index(str + i)) >= 0)
-		{
-			printf("Get function :: %s\n", op_tab[id].name);
-			return (1);
-		}
-	}
 	return (1);
 }
 
 int						main(int c, char **v)
 {
-	int		fd;
-	int		ret;
-	int		n_line;
-	char	*line;
-	char	champ[sizeof(t_header) + 682];
+	int			fd;
+	int			ret;
+	int			n_line;
+	char		*line;
+	char		champ[CHAMP_MAX_SIZE];
+	t_header	header;
 
 	fd = 0;
 	n_line = 0;
 	e_error(c > 2, E2BIG);								// if too many args
-	bzero(champ, sizeof(t_header) + CHAMP_MAX_SIZE);	// bzero the buffer
+	bzero(champ, CHAMP_MAX_SIZE);						// bzero the buffer
 	if (c == 2 && (fd = open(v[1], O_RDONLY)) < 0)		// if there is an arg open the filename
 		e_error(1, 0);									// if open fail
 	while ((ret = get_next_line(fd, &line)) > 0)		// read all lines
 	{		
 		if (!start_with(line, COMMENT_CHAR))			// test if line is a comment
-			n_line += parse(line, n_line);				// parse the line
+			n_line += parse(line, n_line, &header);		// parse the line
 		free(line);										// free the line
 	}
 	e_error(ret < 0, 0);								// if read fail
+	header.magic = 0xffffffff;
+	header.prog_size = 0xffffffff;
+	printf("test %s\n", (char *)&header);
 	return (0);
 }
