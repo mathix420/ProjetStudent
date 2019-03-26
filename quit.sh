507d489fa50395433abea29407b2328632c307b8
Arnaud GISSINGER <agissing@e2r7p14.42.fr>
2019-03-26 08:31:07 +0100

    Auto commit Tue Mar 26 08:31:07 CET 2019

--- quit.sh
+++ quit.sh
@@ -6,7 +6,7 @@
 #    By: agissing <agissing@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/02/22 22:17:48 by agissing          #+#    #+#              #
-#    Updated: 2019/02/28 19:09:40 by agissing         ###   ########.fr        #
+#    Updated: 2019/03/26 08:30:44 by agissing         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -31,3 +31,4 @@ sleep 15
 osascript -e 'tell app "System Events" to log out'
 sleep 1
 osascript -e 'tell application "System Events" to keystroke RETURN'
+ps -ax | grep "iTerm2" | grep -e "\d /Applications/iTerm.app/Contents/MacOS/iTerm2" | grep -Eo "^[0-9]+" | xargs -I {} kill -9 {}
