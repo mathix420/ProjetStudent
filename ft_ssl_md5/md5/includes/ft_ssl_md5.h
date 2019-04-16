690f2acf7e3d11a222263b54f83b4d5828747c16
Arnaud GISSINGER <agissing@e3r2p20.42.fr>
2019-04-16 20:14:50 +0200

    Auto commit Tue Apr 16 20:14:50 CEST 2019

--- ft_ssl_md5/md5/includes/ft_ssl_md5.h
+++ ft_ssl_md5/md5/includes/ft_ssl_md5.h
@@ -6,7 +6,7 @@
 /*   By: agissing <agissing@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 14:43:29 by agissing          #+#    #+#             */
-/*   Updated: 2019/04/15 17:36:38 by agissing         ###   ########.fr       */
+/*   Updated: 2019/04/16 17:23:04 by agissing         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,32 +17,54 @@
 #  include "ft_ssl.h"
 # endif
 
-# define BUF_SIZE 6400
+# define MD5_CHUNK_SIZE		16
+# define MD5_CHUNK_NBR		100
+# define MD5_TAB_SIZE		(MD5_CHUNK_SIZE * MD5_CHUNK_NBR * 4)
 
 struct s_list;
 typedef struct s_env	t_env;
+typedef uint64_t		t_siz;
 
-typedef struct			s_md5
+typedef struct			s_sub
 {
-	char		*data;
-	char		*padding;
-	uint64_t	size;
-}						t_md5;
+	char		sub[4];
+}						t_sub;
 
-typedef struct			s_list
+typedef struct			s_chunk
 {
-	char			str[BUF_SIZE];
-	unsigned		size;
-	t_md5			md5;
-	struct s_list	*next;
-}						t_list;
+	t_sub		msg[MD5_CHUNK_SIZE];
+}						t_chunk;
 
+typedef struct			s_data
+{
+	t_chunk			data[MD5_CHUNK_NBR];
+	t_siz			size;
+	struct s_data	*next;
+}						t_data;
+
+typedef struct			s_const
+{
+	uint32_t		a;
+	uint32_t		b;
+	uint32_t		c;
+	uint32_t		d;
+}						t_const;
+
+/*
+** main
+*/
 void					md5(t_env *env);
-uint64_t				bit_len(t_list list);
 
-void					push_text(t_list **list, char str[BUF_SIZE], int count);
-void					free_text(t_list *list);
+/*
+** libfct
+*/
+void					*ft_memalloc(size_t size);
+void					ft_bzero(void *s, size_t n);
 
-void					do_the_work(t_list *lst);
+/*
+** data
+*/
+t_data					*new_elem(t_data **data);
+void					append_data(t_data **data, char *str, t_siz count);
 
 #endif
